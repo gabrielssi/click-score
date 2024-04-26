6c8919b386ad437503d9e50f43c11ece9e8f9612
Gabriel Souza da Silva <107503265+gabrielssi@users.noreply.github.com>
2024-04-26 18:56:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let scoreWins = 50;
 function ExibaScore() {
   let escreva = document.querySelector('h1');
   if (score == scoreWins) {
-    escreva.innerHTML = `Você ganhou!`;
+    escreva.innerHTML = `Você ganhou, Anna!`;
     score = 0;
   } else { escreva.innerHTML = `${score}` };
 }
